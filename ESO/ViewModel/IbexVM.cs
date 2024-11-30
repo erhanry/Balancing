931eca8aada9312cc36ad693b9c806cff3cf9e4a
erhanry <erhan.ysuf@gmail.com>
2024-11-30 19:30:46 +0200

    Add View Model Class

---
+++ ESO/ViewModel/IbexVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESO.Model;
+
+namespace ESO.ViewModel
+{
+    class IbexVM: Utilities.ViewModelBase
+    {
+        private readonly PageModel _pageModel;
+
+        public IbexVM()
+        {
+            _pageModel = new PageModel();
+        }
+    }
+}
---
+++ ESO/ViewModel/NavigationVM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESO.Utilities;
+using System.Windows.Input;
+
+namespace ESO.ViewModel
+{
+    class NavigationVM : ViewModelBase
+    {
+        private object? _currentView;
+        public object CurrentView
+        {
+            get { return _currentView!; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public ICommand DashboardCommand { get; set; }
+        public ICommand IbexCommand { get; set; }
+
+        private void Dashboard(object obj) => CurrentView = new DashboardVM();
+        private void Ibex(object obj) => CurrentView = new IbexVM();
+
+        public NavigationVM()
+        {
+            DashboardCommand = new RelayCommand(Dashboard);
+            IbexCommand = new RelayCommand(Ibex);
+
+            // Startup Page
+            CurrentView = new DashboardVM();
+        }
+    }
+}
